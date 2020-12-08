d2e6a3259bc50fece005ed622b39e2e6fa757413
aocar <aocarroll@gmail.com>
2020-12-08 17:30:09 +0000

    test

---
+++ src/naughtsandcrossesgui/NaughtsAndCrossesGUI.java
@@ -0,0 +1,19 @@
+
+package naughtsandcrossesgui;
+
+import java.awt.Toolkit;
+
+public class NaughtsAndCrossesGUI {
+
+    public static void main(String[] args) {
+        
+        Game game = new Game();
+        
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int xSize = (int)tk.getScreenSize().getWidth();
+        int ySize = (int)tk.getScreenSize().getHeight();
+        game.setSize(xSize,ySize);
+        game.setVisible(true); 
+        
+    }
+}
